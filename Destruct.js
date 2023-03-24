2d71ffbaf44281d692e62b234afa757c271bf589
LynixPlayz <82721356+LynixPlayz@users.noreply.github.com>
2023-03-24 11:07:34 -0500

    Update Destruct.js

--- Destruct.js
+++ Destruct.js
@@ -2,7 +2,9 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.4.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$(document).keypress(function (event) {
-  
-            alert('You pressed a key');
-        });
+$(document).keydown(function(){
+  if(event.which == 13)
+  {
+    document.body.remove();
+  }
+});
